35cbd8c3b68535e75d6a50074a09f452e7325a7b
sabicera <117107103+sabicera@users.noreply.github.com>
2022-11-28 06:20:58 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,37 @@
 // This is a Header section //
 
 header.innerHTML = `
-    <header>
-        <a href="home.html" class="">
-            <img src="#">
-        </a>
-        <nav>
-            <ul>
-                <li><a href="home.html">Home</a></li>
-                <li><a href="downloads.html">Downloads</a></li>
-                <li><a href="guides.html">Guides</a></li>
-            </ul>
-        </nav>
-    </header>
+<header>
+    <nav>
+        <a href="/index.html"><h1 class="logo">Ramnuda&copy;</h1></a>
+        <a href="" title="menu" class="toggle-button"><span class="bar"></span><span class="bar"></span><span class="bar"></span></a>
+        <ul class="menu">
+            <li><a href="">Downloads</a>
+                <ul class="submenu">
+                    <li><a href="/downloads/windows.html">Windows ISO</a></li>
+                    <li><a href="/downloads/drivers.html">GPU Drivers</a></li>
+                    <li><a href="/downloads/programs.html">Programs</a></li>
+                </ul>
+            </li>
+            <li><a href="">Guides</a>
+                <ul class="submenu">
+                    <li><a href="/guides/psuefficiency.html">PSU Efficiency</a></li>
+                    <li><a href="/guides/win-install.html">Windows setup</a></li>
+                    <li><a href="/guides/biosupdate.html">Bios Update</a></li>
+                    <li><a href="/guides/random.html">Random</a></li>
+                </ul>
+            </li>
+            <li><a href="/cdkeys.html">Buy CDKeys</a></li>
+        </ul>
+    </nav>
+</header>
 `;
 
 // This a a Footer section //
 
 footer.innerHTML = `
-    <footer>
-    &copy; 2022 | Ramnuda
-    </footer>
+<footer>
+    <h1 class="footer">Ramnuda &copy; 2022  |  All rights reserved</h1>
+</footer>
+<a href="" title="Go up" class="to-top"><i class="fa-solid fa-chevron-up"></i></a>
 `;
