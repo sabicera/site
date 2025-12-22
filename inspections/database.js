8eabf22aed45faf87e36e8af9b39fc4516105540
sabicera <117107103+sabicera@users.noreply.github.com>
2025-12-22 03:28:38 +0200

    Create database.js

---
+++ inspections/database.js
@@ -0,0 +1,29 @@
+window.formatVesselNameWithLink = function(text) {
+    // Regex looks for *NAME* -
+    const regex = /\*(.*?)\*\s*-/;
+    const match = text.match(regex);
+
+    if (match) {
+        const vesselName = match[1].trim().toUpperCase();
+        const shipId = VESSEL_ID_DATABASE[vesselName];
+
+        if (shipId) {
+            // Live map link with zoom level 10
+            const url = `https://www.marinetraffic.com/en/ais/home/shipid:${shipId}/zoom:8`;
+            const linkHtml = `<a href="${url}" target="_blank" class="vessel-link">${match[1].trim()}</a>`;
+            return text.replace(`*${match[1]}*`, linkHtml);
+        }
+    }
+    return text;
+};
+
+const VESSEL_ID_DATABASE = {
+    "MSC ZONDA III": "156432",
+    "MSC PRETORIA II": "757962",
+    "MSC AMALFI": "173852",
+    "MSC VIGO": "758696",
+    "ARTEMIS": "6291945",
+    "EUROPE": "144594",
+    "VALUE": "2657",
+
+};
